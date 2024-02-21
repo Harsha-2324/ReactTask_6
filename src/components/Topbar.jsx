2caacce35c1070ceb318a536aaf2eee261029f45
Harsha-2324 <rharshva@gmail.com>
2024-02-22 00:36:22 +0530

    crud commit

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -6,18 +6,27 @@ import { useNavigate } from 'react-router-dom';
 function Topbar() {
     let navigate = useNavigate();
   return (
-    <Navbar expand="lg" className="bg-success">
+    <Navbar expand="lg" className="bg-secondary">
       <Container>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        
+
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link onClick={()=>navigate('/')}><h5><b>Dashboard</b></h5></Nav.Link>
-            <Nav.Link onClick={()=>navigate('/create')}><h5><b>Create</b></h5></Nav.Link>
+            <Nav.Link onClick={() => navigate("/")}>
+              <h5>
+                <b>Dashboard</b>
+              </h5>
+            </Nav.Link>
+            <Nav.Link onClick={() => navigate("/create")}>
+              <h5>
+                <b>Create</b>
+              </h5>
+            </Nav.Link>
           </Nav>
           <Navbar.Collapse className="justify-content-end">
             <Navbar.Text>
-              <h5>Signed in as :</h5><h4>Harshavardhan Rajinikanth</h4>
+              <h5>Signed in as :</h5>
+              <h4>Harshavardhan Rajinikanth</h4>
             </Navbar.Text>
           </Navbar.Collapse>
         </Navbar.Collapse>
